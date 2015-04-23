aeb6bd35d9638d088c54f97a1aca1ba87eb6aec1
hemant_bcs11 <hemant_bcs11@ssl-47.ssl.cse>
2015-04-23 23:19:45 +0530

    GEXF exporter is created to export the data in SQL for graph visualization.

---
+++ gexfexporter.c
@@ -0,0 +1,60 @@
+#include<mysql.h>
+#include<my_global.h>
+#include<stdio.h>
+
+int main() {
+	FILE * fp = fopen("provenance.gexf","w");
+
+	MYSQL *con = mysql_init(NULL);
+	MYSQL_RES *result;
+	MYSQL_ROW row;
+	if(mysql_real_connect(con, "localhost", "provenance", "provenance", "provenance",0, NULL,0) == NULL) {
+		printf("%s",mysql_error(con));
+		mysql_close(con);
+	}
+
+	//printing header informations
+	fprintf(fp, "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n");
+
+	//printing gexf information
+	fprintf(fp, "<gexf xmlns=\"http://www.gexf.net/1.2draft\" version=\"1.2\">\n");
+
+	//printing graph information
+	fprintf(fp, "<graph mode=\"static\" defaultedgetype=\"directed\">\n");
+
+	//print node information
+	fprintf(fp, "<nodes>\n");
+
+	mysql_query(con, "select * from window_info;");
+
+	result = mysql_store_result(con);
+	while((row = mysql_fetch_row(result))) {
+		fprintf(fp, "<node id=\"%s\" label=\"%s\" />\n", row[0], row[2]);
+	}
+
+	//end of node information
+	fprintf(fp, "</nodes>\n");
+
+	//print edge information
+	fprintf(fp, "<edges>\n");
+    mysql_query(con, "select *,COUNT(*) from just_flow group by blueprint;");
+    result = mysql_store_result(con);
+	int i=0;
+	while((row = mysql_fetch_row(result))) {
+        fprintf(fp, "<edge id=\"%d\" source=\"%s\" target=\"%s\" weight=\"%s\" />\n", i, row[0], row[1], row[3]);
+		i++;
+    }
+
+
+	//end of edge information
+	fprintf(fp, "</edges>\n");
+
+	//end of grpah information
+	fprintf(fp, "</graph>\n");
+
+	//end of gexf inforamtion
+	fprintf(fp, "</gexf>");
+
+
+	return 0;
+}
--- provenance.sql
+++ provenance.sql
@@ -16,14 +16,6 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Current Database: `provenance`
---
-
-CREATE DATABASE /*!32312 IF NOT EXISTS*/ `provenance` /*!40100 DEFAULT CHARACTER SET latin1 */;
-
-USE `provenance`;
-
---
 -- Table structure for table `copy_info`
 --
 
@@ -38,7 +30,7 @@ CREATE TABLE `copy_info` (
   PRIMARY KEY (`data_id`),
   KEY `owner_id` (`owner_id`),
   CONSTRAINT `copy_info_ibfk_1` FOREIGN KEY (`owner_id`) REFERENCES `window_info` (`window_id`) ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -47,8 +39,39 @@ CREATE TABLE `copy_info` (
 
 LOCK TABLES `copy_info` WRITE;
 /*!40000 ALTER TABLE `copy_info` DISABLE KEYS */;
+INSERT INTO `copy_info` VALUES (10,15,'IBM\'s Watson learning computer system isn\'t just content with making the occasional meal -- it has a whole slew of recipes lined up. The tech company is launching Cognitive Cooking with Chef Watson, a cookbook based on Watson\'s knack for combining food in a way that ','2015-04-23 04:13:06'),(11,17,'This browser window is used to keep your authentication ','2015-04-23 04:13:23'),(12,15,'This is a test data','2015-04-23 10:29:56');
 /*!40000 ALTER TABLE `copy_info` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Temporary table structure for view `data_flow`
+--
+
+DROP TABLE IF EXISTS `data_flow`;
+/*!50001 DROP VIEW IF EXISTS `data_flow`*/;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
+/*!50001 CREATE TABLE `data_flow` (
+  `Owner` tinyint NOT NULL,
+  `Requestor` tinyint NOT NULL,
+  `Data_ID` tinyint NOT NULL
+) ENGINE=MyISAM */;
+SET character_set_client = @saved_cs_client;
+
+--
+-- Temporary table structure for view `just_flow`
+--
+
+DROP TABLE IF EXISTS `just_flow`;
+/*!50001 DROP VIEW IF EXISTS `just_flow`*/;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
+/*!50001 CREATE TABLE `just_flow` (
+  `owner` tinyint NOT NULL,
+  `requestor` tinyint NOT NULL,
+  `blueprint` tinyint NOT NULL
+) ENGINE=MyISAM */;
+SET character_set_client = @saved_cs_client;
 
 --
 -- Table structure for table `paste_info`
@@ -74,6 +97,7 @@ CREATE TABLE `paste_info` (
 
 LOCK TABLES `paste_info` WRITE;
 /*!40000 ALTER TABLE `paste_info` DISABLE KEYS */;
+INSERT INTO `paste_info` VALUES (16,10,'2015-04-23 04:13:12'),(18,11,'2015-04-23 04:13:51'),(16,12,'2015-04-23 10:31:47'),(16,12,'2015-04-23 11:09:16');
 /*!40000 ALTER TABLE `paste_info` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -90,7 +114,7 @@ CREATE TABLE `window_info` (
   `window_title` varchar(50) NOT NULL,
   PRIMARY KEY (`window_id`),
   UNIQUE KEY `window` (`window`,`window_title`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -99,8 +123,47 @@ CREATE TABLE `window_info` (
 
 LOCK TABLES `window_info` WRITE;
 /*!40000 ALTER TABLE `window_info` DISABLE KEYS */;
+INSERT INTO `window_info` VALUES (17,56623105,'Firewall Authentication Keepalive Window - Google '),(15,56623105,'IBM\'s Watson cognitive computer has whipped up a c'),(16,67108997,'*Untitled Document 1 - gedit'),(18,69206049,'Document1 - Writer');
 /*!40000 ALTER TABLE `window_info` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Final view structure for view `data_flow`
+--
+
+/*!50001 DROP TABLE IF EXISTS `data_flow`*/;
+/*!50001 DROP VIEW IF EXISTS `data_flow`*/;
+/*!50001 SET @saved_cs_client          = @@character_set_client */;
+/*!50001 SET @saved_cs_results         = @@character_set_results */;
+/*!50001 SET @saved_col_connection     = @@collation_connection */;
+/*!50001 SET character_set_client      = utf8 */;
+/*!50001 SET character_set_results     = utf8 */;
+/*!50001 SET collation_connection      = utf8_general_ci */;
+/*!50001 CREATE ALGORITHM=UNDEFINED */
+/*!50013 DEFINER=`provenance`@`localhost` SQL SECURITY DEFINER */
+/*!50001 VIEW `data_flow` AS select `C`.`owner_id` AS `Owner`,`P`.`requestor_id` AS `Requestor`,`P`.`data_id` AS `Data_ID` from (`copy_info` `C` join `paste_info` `P`) where (`C`.`data_id` = `P`.`data_id`) */;
+/*!50001 SET character_set_client      = @saved_cs_client */;
+/*!50001 SET character_set_results     = @saved_cs_results */;
+/*!50001 SET collation_connection      = @saved_col_connection */;
+
+--
+-- Final view structure for view `just_flow`
+--
+
+/*!50001 DROP TABLE IF EXISTS `just_flow`*/;
+/*!50001 DROP VIEW IF EXISTS `just_flow`*/;
+/*!50001 SET @saved_cs_client          = @@character_set_client */;
+/*!50001 SET @saved_cs_results         = @@character_set_results */;
+/*!50001 SET @saved_col_connection     = @@collation_connection */;
+/*!50001 SET character_set_client      = utf8 */;
+/*!50001 SET character_set_results     = utf8 */;
+/*!50001 SET collation_connection      = utf8_general_ci */;
+/*!50001 CREATE ALGORITHM=UNDEFINED */
+/*!50013 DEFINER=`provenance`@`localhost` SQL SECURITY DEFINER */
+/*!50001 VIEW `just_flow` AS select `data_flow`.`Owner` AS `owner`,`data_flow`.`Requestor` AS `requestor`,concat(`data_flow`.`Owner`,'+',`data_flow`.`Requestor`) AS `blueprint` from `data_flow` */;
+/*!50001 SET character_set_client      = @saved_cs_client */;
+/*!50001 SET character_set_results     = @saved_cs_results */;
+/*!50001 SET collation_connection      = @saved_col_connection */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -111,4 +174,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2015-04-13 22:49:35
+-- Dump completed on 2015-04-23 23:15:09
